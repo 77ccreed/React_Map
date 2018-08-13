cf47430446410394d45d857fa3c55640ad42922c
Kiur Jullis <kiur.jullis@gmail.com>
2018-08-13 09:18:40 +0300

    add locations.js file

---
+++ my-app/src/data/locations.js
@@ -0,0 +1,22 @@
+export const locations = [
+  {
+    name: 'Conspirator Bar',
+    coords: { lat: 57.77545740389894, lng: 26.040790990543993 }
+  },
+  {
+    name: 'Sinel',
+    coords: { lat: 57.782337, lng: 26.053679 }
+  },
+  {
+    name: 'Restoran Metsis',
+    coords: { lat: 57.780649660235774, lng: 26.04978000759657 }
+  },
+  {
+    name: 'Lilli restoran',
+    coords: { lat: 57.778025809989124, lng: 26.038864631540076 }
+  },
+  {
+    name: 'Vabaduse Kohvik',
+    coords: { lat: 57.775024, lng: 26.044881 }
+  }
+];
