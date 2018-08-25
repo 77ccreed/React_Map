0d7b1312d1c3fefadd674e795bf03f1daaf65ef0
Kiur Jullis <kiur.jullis@gmail.com>
2018-08-25 11:13:53 +0300

    add InputList Component

---
+++ my-app/src/components/InputList.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import {
+  InputGroup,
+  InputGroupButtonDropdown,
+  Input,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+import PropTypes from 'prop-types';
+import escapeRegExp from 'escape-string-regexp';
+
+export default class InputList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleDropDown = this.toggleDropDown.bind(this);
+
+    this.state = {
+      dropdownOpen: true,
+      query: "",
+      searchedVenue: this.props.venuesList
+    };
+  }
+
+  updateQuery = (query) => {
+
+    this.setState({
+      query: query,
+      dropdownOpen: true
+    })
+    this.handleInput(query)
+  }
+
+  handleInput = (query) => {
+    let searchVenue
+
+    if (query) {  
+           
+      const match = new RegExp(escapeRegExp(this.state.query), 'i');
+
+      // Add location to the array if its title match the query 
+      searchVenue = this.props.venuesList.filter(venue =>
+        match.test(venue.venue.name)
+      );
+      this.setState({
+        query: query,
+         searchedVenue: searchVenue 
+        });
+    }
+    else { 
+      searchVenue = this.props.venuesList;
+      this.setState({
+        query: "",
+        searchedVenue: searchVenue 
+      });
+    } 
+    //this.props.onFilterLocation(searchVenue);
+    //this.props.onUnselectLocation();
+  };
+
+
+  toggleDropDown() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
+  render() {
+    const { searchedVenue } = this.state;
+    return ( 
+        <InputGroup>
+          <Input 
+          placeholder="Add location name"
+          id="input"
+          value={this.state.query}
+          onChange={(event) => this.updateQuery(event.target.value)}
+          />
+          <InputGroupButtonDropdown addonType="append" isOpen={this.state.dropdownOpen} toggle={this.toggleDropDown}>
+            <DropdownToggle caret>
+              Choose a Dinery
+            </DropdownToggle>
+            <DropdownMenu>
+            {searchedVenue.map((venue,id) => (
+              <DropdownItem
+                key={id}
+                className="list-items"
+              >
+                {venue.venue.name}
+                </DropdownItem>
+            ))}
+            </DropdownMenu>
+          </InputGroupButtonDropdown>
+        </InputGroup>      
+    );
+  }
+}
+
+InputGroup.propTypes = {
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  size: PropTypes.string,
+  className: PropTypes.string
+};
